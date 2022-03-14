66ee0f83e70a1c83d91e6b47d38df9f5716b1f6d
DustinSchouten <dustinschouten2@gmail.com>
2022-03-14 14:40:01 +0100

    first_commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,8 @@
 /*** Fetching data -> refactor into module later ***/
-const main = document.querySelector('main');
+const ul = document.querySelector('ul');
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
-const query = 'tolkien';
+const query = 'voeding';
 const key = '1e19898c87464e239192c8bfe422f280';
 const secret = '4289fec4e962a33118340c888699438d';
 const detail = 'Default';
@@ -28,16 +28,14 @@ function render(data) {
   console.dir(results);
   results.forEach((item, i) => {
     const html = `
-            <article>
+            <li>
               <h2>${item.titles[0]}</h2>
               <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
               <img src="${
                 item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
               }">
-            </article>
+            </li>
           `;
-    main.insertAdjacentHTML('beforeend', html);
+    ul.insertAdjacentHTML('beforeend', html);
   });
 }
-
-console.log(true)
