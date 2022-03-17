a82fab651a1186128427fc25d69c92a02a9e66b7
DustinSchouten <dustinschouten2@gmail.com>
2022-03-17 15:46:26 +0100

    zoek_op_titel filter toegevoegd

--- javascript/modules/filters.js
+++ javascript/modules/filters.js
@@ -1,5 +1,8 @@
 import { setAmountOfSearchResults } from './search_results.js'
 
+const input_filter = document.querySelector('#input');
+input_filter.addEventListener('keyup',setFilters);
+
 const category_filter = document.querySelector('#categorie');
 category_filter.addEventListener('change',setFilters);
 
@@ -7,6 +10,7 @@ const language_filter = document.querySelector('#taal');
 language_filter.addEventListener('change',setFilters);
 
 export function setFilters() {
+  const input_filter_value = input_filter.value;
   const language_filter_value = language_filter.value;
   const category_filter_value = category_filter.value;
   const book_items = document.querySelectorAll('.book_item');
@@ -14,6 +18,15 @@ export function setFilters() {
   book_items.forEach((book_item,idx) => {
     book_item.classList.remove('invisible') // By default, make all the book items visible;
   });
+
+  if (input_filter_value != '') { // If the filter is activated;
+    book_items.forEach((book_item,idx) => {
+      const title = book_item.querySelector('h2').textContent.toUpperCase();
+      if (title.includes(input_filter_value.toUpperCase()) == false) { // If the title doesn't match with the selected filter;
+        book_item.classList.add('invisible');
+      }
+    });
+  }
 
   if (language_filter_value != 'leeg') { // If the filter is activated
     book_items.forEach((book_item,idx) => {
@@ -24,7 +37,7 @@ export function setFilters() {
     });
   }
 
-  if (category_filter_value != 'leeg') {
+  if (category_filter_value != 'leeg') { // If the filter is activated
     book_items.forEach((book_item,idx) => {
       const category = book_item.querySelector('.category').textContent.split(': ')[1];
       if (category != category_filter_value) {
